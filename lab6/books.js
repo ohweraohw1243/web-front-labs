f116d3c3d611408e11245687e17790a639c6dca2
Волков Даниил <v01k0v.d4ny@yandex.ru>
2025-05-16 19:17:21 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,12 @@
+const books = [
+    { author: 'Лев Толстой', name: 'Война и мир', year: 1869, price: 500, bestseller: true, inStock: true },
+    { author: 'Федор Достоевский', name: 'Преступление и наказание', year: 1866, price: 450, bestseller: true, inStock: false },
+    { author: 'Александр Пушкин', name: 'Евгений Онегин', year: 1833, price: 300, bestseller: false, inStock: true },
+    { author: 'Николай Гоголь', name: 'Мертвые души', year: 1842, price: 400, bestseller: false, inStock: false },
+    { author: 'Антон Чехов', name: 'Рассказы', year: 1900, price: 350, bestseller: true, inStock: true },
+    { author: 'Михаил Булгаков', name: 'Мастер и Маргарита', year: 1967, price: 550, bestseller: true, inStock: true },
+    { author: 'Иван Тургенев', name: 'Отцы и дети', year: 1862, price: 380, bestseller: false, inStock: true },
+    { author: 'Владимир Набоков', name: 'Лолита', year: 1955, price: 600, bestseller: true, inStock: false },
+    { author: 'Франс Кафка', name: 'Процесс', year: 1925, price: 420, bestseller: false, inStock: true },
+    { author: 'Джордж Оруэлл', name: '1984', year: 1949, price: 480, bestseller: true, inStock: true }
+];
